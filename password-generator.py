4e8f97c8a4e7d4c5ac52c2aee8a592c6a2e752ae
Yanis Lemb <yanismail1206@gmail.com>
2023-01-08 21:39:34 +0100

    First commit

---
+++ password-generator.py
@@ -0,0 +1,32 @@
+import random 
+
+letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbol = "§£$%@#{}[]()!"
+character = letter + letter.lower() + number + symbol
+character_without_symbols = letter + letter.lower() + number
+
+while True:
+    pass_length = int(input("Entrez la longueur du mot de passe : "))
+    pass_number = int(input("Entrez le nombre de mot de passe à générer : "))
+    special_character = str(input("Mot de passe avec caractères spéciaux ou non ? : "))
+
+    if special_character == "oui":
+        for i in range(0,pass_number):
+            passw = ""
+            for i in range(0,pass_length):
+                pass_character = random.choice(character)
+                passw = passw + pass_character
+            print("Votre mot de passe est: ", passw)
+
+    elif special_character == "non":
+        for i in range(0,pass_number):
+            passw = ""
+            for i in range(0,pass_length):
+                pass_character = random.choice(character_without_symbols)
+                passw = passw + pass_character
+            print("Votre mot de passe est: ", passw)
+
+
+    elif special_character != "oui"| "non":
+        print("Error")
